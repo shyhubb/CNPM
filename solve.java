e1ab97bd437a2e9c15cc033f5ab04f1071caf787
shyhubb <shyhubel@gmai.com>
2025-02-25 09:54:16 +0700

    Add solve.java

---
+++ solve.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class ictu{
+    public static int Ictu(int x ){
+       int res = 0;
+       for(int i = 1 ; i <= x ; i++){
+        res += i * 11;
+       }
+       return res;
+    }
+}
+public class solve {
+
+public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Nhap so N");
+    int x  = scanner.nextInt();
+    System.out.println("S = " + ictu.Ictu(x));
+    scanner.close();
+}
+}
